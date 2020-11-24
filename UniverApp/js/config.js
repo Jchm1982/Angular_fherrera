200b9e52bbb8c6d401de1fa1bc683461450e2848
JuanC <maijujoshproyects@gmail.com>
2020-11-23 23:03:21 -0600

    Ca231120_2303

---
+++ UniverApp/js/config.js
@@ -0,0 +1,16 @@
+app.config(function($routeProvider){
+	$routeProvider
+		.when('/',{
+			templateUrl: 'parciales/home.html'
+		})
+		.when('/profesores',{
+			templateUrl: 'parciales/profesores.html'
+		})
+		.when('/alumnos',{
+			templateUrl: 'parciales/alumnos.html'
+		})
+		.otherwise({
+			redirectTo: '/'
+		});
+
+});
